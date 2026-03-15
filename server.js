aeb111bc9b65c036c5e2592d2e4c41007434f76e
Nitintomar0 <164238902+Nitintomar0@users.noreply.github.com>
2026-03-15 21:52:23 +0530

    Add routes for auth and wallpapers

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use(express.urlencoded({ extended: true }));
  * ROUTES
  */
 app.use("/api/payments", require("./routes/payment.routes"));
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/wallpapers", require("./routes/wallpaper.routes"));
 
 /**
  * HEALTH CHECK
