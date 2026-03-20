17dc73c00a3742996986bd8662624c58a4792e1d
nitintomar1111 <nitintomar874281@gmail.com>
2026-03-20 14:39:58 +0530

    Create purchase.routes.js

---
+++ routes/purchase.routes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const Purchase = require("../models/Purchase");
+
+/* SAVE PURCHASE */
+router.post("/add", async (req, res) => {
+  const { userId, wallpaperId } = req.body;
+
+  try {
+    const exists = await Purchase.findOne({ userId, wallpaperId });
+
+    if (!exists) {
+      await Purchase.create({ userId, wallpaperId });
+    }
+
+    res.json({ success: true });
+
+  } catch (err) {
+    res.status(500).json({ success: false });
+  }
+});
+
+/* GET USER PURCHASES */
+router.get("/:userId", async (req, res) => {
+  try {
+    const purchases = await Purchase.find({
+      userId: req.params.userId
+    });
+
+    res.json(purchases);
+
+  } catch (err) {
+    res.status(500).json([]);
+  }
+});
+
+module.exports = router;
