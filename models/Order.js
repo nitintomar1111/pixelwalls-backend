9efee465c5715fe6c06071c7dbcd88cf9c7a6544
nitintomar1111 <nitintomar874281@gmail.com>
2026-03-27 19:31:02 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   orderId: String,
-  wallpaperId: String
+  wallpaperId: String,
+
+  userId: String,   // ✅ ADD THIS
+  status: {
+    type: String,
+    default: "PENDING"
+  }
 });
 
 module.exports = mongoose.model("Order", orderSchema);
