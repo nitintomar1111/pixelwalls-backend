7e0c32fec2bb46a62a67e71dd86db3b861fb739f
nitintomar1111 <nitintomar874281@gmail.com>
2026-03-27 23:04:40 +0530

    Update payment.routes.js

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -29,9 +29,9 @@ router.post("/create-order", async (req, res) => {
 
       // ✅ VERY IMPORTANT (REDIRECT FIX)
       order_meta: {
-        return_url:
-  "https://pixel-walls.com/purchased.html?order_id={order_id}"
-      },
+  return_url:
+    "https://pixel-walls.com/index.html?order_id={order_id}"
+},
 
       customer_details: {
         customer_id: "cust_" + uuidv4(),
